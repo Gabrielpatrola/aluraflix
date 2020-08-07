026aef633e9be84555b59ef92d6eb989e4b6bcf9
Gabriel Augusto Almeida <almeidagabrielaugusto@gmail.com>
2020-08-06 22:41:56 -0400

    Link e to

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import CadastroVideo from './pages/cadastro/Video'
+import Home from './pages/Home'
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+const PageNotFound = () => (<div>404</div>);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={Home} exact />
+      <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route component={PageNotFound} />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
---
+++ src/pages/cadastro/Video/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function CadastroVideo() {
+  return (
+    <div style={{ background: "#141414" }}>
+      Página de cadastro de video
+    </div>
+  );
+}
+
+export default CadastroVideo;
